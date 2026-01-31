d6669a4538b81f99f3217bd9857b94a1ba532c3e
Joanna Li <jl561@rice.edu>
2026-01-31 11:05:52 -0600

    Fixed week 1 retry.sh

--- week1-jl561/retry.sh
+++ week1-jl561/retry.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
 RETRY=1
-RUN_COUNT=0
+RUNS=0
+START=$(date +%s)
 
-while [ $RETRY = 1]
+while [ true ] 
 do 
-    
+   ((RUNS++))
+   #Must run the line to be evaluated RIGHT BEFORE before checking $?, or save $?
+   bash "$1" > stdout.log 2> stderr.log #Redirect output
+   if [ $? -ne 0 ]; #If script had an error
+   then 
+      echo "Statistics:"
+      echo "Successful runs: "$(($RUNS - 1))"" #Use $(( )) to expand arithmetic
+      echo "Failed on run: $RUNS"
+      break
+   fi
+   echo "Run $RUNS: success" 
 done
+END=$(date +%s)
+echo "- Total time: "$(($END - $START))" seconds" 
+echo "Error output:"
+cat  stderr.log 
+echo "Standard output from failed run:"
+cat  stdout.log 
